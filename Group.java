49cf5f3f2151278e0085123bfb67b3c62d39f4f4
BorodachGolovach <57922209+BorodachGolovach@users.noreply.github.com>
2019-12-02 21:52:19 +0300

    Еще 5 задание

--- Group.java
+++ Group.java
@@ -2,7 +2,7 @@ package Model;
 import Model.Student;
 
 public class Group {
-    public Student [] a = new Student[5];
+    public Student [] = new Student[5];
     public int id;
     public String students;
     public String programmingLanguage;
@@ -10,7 +10,7 @@ public class Group {
         this.id=5;
         this.students
     }
-    public void addstudent() {
+    public void addstudent(String student) {
         System.out.println(firstName);
         System.out.println(lastName);
         System.out.println(age);
--- Main2.java
+++ Main2.java
@@ -7,12 +7,4 @@ public class Main2 {
         Student student = new Student();
         student.printInformation();
     }
-
-    public static class MainGroup {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Input :");
-            int j = scanner.nextInt();
-            int[] a;
-    }
 }
